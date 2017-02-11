e1a3a57ce85f01f68d8157fc8af51bb3d1d1f6fb
bankuru <bankuru.prasannna@inmobi.com>
2017-02-12 01:24:42 +0530

    added Ping api

--- src/main/java/org/johnmusic/service/Constants.java
+++ src/main/java/org/johnmusic/service/Constants.java
@@ -5,8 +5,8 @@ package org.johnmusic.service;
  */
 public class Constants {
     //ec2-52-66-134-60.ap-south-1.compute.amazonaws.com
-    public static final String DB_SELECT_ENDPOINT = "http://ec2-52-66-134-60.ap-south-1.compute.amazonaws.com:8983/solr/strandsContent/select";
-    public static final String DB_UPDATE_ENDPOINT = "http://ec2-52-66-134-60.ap-south-1.compute.amazonaws.com:8983/solr/strandsContent/update?commit=true";
+    public static final String DB_SELECT_ENDPOINT = "http://ec2-35-154-161-135.ap-south-1.compute.amazonaws.com:8983/solr/JohnMusicClasses/select";
+    public static final String DB_UPDATE_ENDPOINT = "http://ec2-35-154-161-135.ap-south-1.compute.amazonaws.com:8983/solr/JohnMusicClasses/update?commit=true";
 
     //Solr params
     public static final String SUCCESS = "success";
@@ -14,9 +14,6 @@ public class Constants {
     public static final String RESPONSE = "response";
     public static final String DOCS = "docs";
     public static final String WT_JSON = "wt=json";
-    public static final String GCMID = "gcmId";
-    public static final String APNSID = "apNsId";
-    public static final String FB_ID = "fbId";
     public static final String ALL = "*";
     public static final String AND = " AND ";
     public static final String SET = "set";
@@ -26,13 +23,7 @@ public class Constants {
     public static final String INSERT_END = "]";
     public static final String TRUE = "true";
     public static final String FALSE = "false";
-    public static final String IOEXCEPTION_ERRORMESSAGE = "IO Exception";
-    public static final int ERRORCODE_IOEXCEPTION = 102;
-    public static final int ERRORCODE_INVALID_INPUT = 103;
-    public static final int ERRORCODE_JSON_EXCEPTION = 101;
-    public static final int ERRORCODE_INVALID_AUTH = 104;
     public static final String ACCEPT_CHARSET = "Accept-Charset";
-    public static final String ERROR_MESSAGE = "errorMessage";
     public static final String NUMFOUND = "numFound";
     public static final String START = "start";
     public static final String ROWS = "rows";
@@ -44,6 +35,12 @@ public class Constants {
 
 
     //Error messages
+    public static final String IOEXCEPTION_ERRORMESSAGE = "IO Exception";
+    public static final int ERRORCODE_IOEXCEPTION = 102;
+    public static final int ERRORCODE_INVALID_INPUT = 103;
+    public static final int ERRORCODE_JSON_EXCEPTION = 101;
+    public static final int ERRORCODE_INVALID_AUTH = 104;
+    public static final String ERROR_MESSAGE = "errorMessage";
 
 
 }
